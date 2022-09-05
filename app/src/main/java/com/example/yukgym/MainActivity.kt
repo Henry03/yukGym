34ad1a4f0ebe43996a3a299049c833d58afbf216
Henry03 <henryyanggg@gmail.com>
2022-09-06 01:24:35 +0700

    Register, Login, and Home page

--- app/src/main/java/com/example/yukgym/MainActivity.kt
+++ app/src/main/java/com/example/yukgym/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.yukgym
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputUsername: TextInputLayout
+    private lateinit var inputPassword: TextInputLayout
+    private lateinit var mainLayout: ConstraintLayout
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTitle("Login")
+        setupHyperlink()
+
+        inputUsername = findViewById(R.id.ilUsername)
+        inputPassword = findViewById(R.id.ilPassword)
+        mainLayout = findViewById(R.id.mainLayout)
+
+        val btnClear: Button
+        val btnLogin: Button
+
+        btnClear = findViewById(R.id.btnClear)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnClear.setOnClickListener {
+            inputUsername.editText?.setText("")
+            inputPassword.editText?.setText("")
+
+            Snackbar.make(mainLayout, "Text Cleared Success", Snackbar.LENGTH_LONG).show()
+        }
+
+        btnLogin.setOnClickListener(View.OnClickListener {
+            val username: String = inputUsername.editText?.getText().toString()
+            val password: String = inputPassword.editText?.getText().toString()
+
+            if(username.isEmpty()){
+                inputUsername.setError("Username must be filled with text")
+            }
+
+            if(password.isEmpty()){
+                inputPassword.setError("Password must be filled with text")
+            }
+
+            if(username == "admin" && password == "yukgym"){
+                val moveHome = Intent(this, MainMenu::class.java)
+                startActivity(moveHome)
+            }else{
+                return@OnClickListener
+            }
+
+
+        })
+    }
+    fun setupHyperlink(){
+        val linkTextView = findViewById<TextView>(R.id.btnSignUp)
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance())
+
+        linkTextView.setOnClickListener(View.OnClickListener {
+            val movetoRegister = Intent(this, Register::class.java)
+            startActivity(movetoRegister)
+        })
     }
 }
--- app/src/main/java/com/example/yukgym/Register.kt
+++ app/src/main/java/com/example/yukgym/Register.kt
@@ -1,11 +1,124 @@
 package com.example.yukgym
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class Register : AppCompatActivity() {
+
+    private lateinit var name: TextInputLayout
+    private lateinit var nik: TextInputLayout
+    private lateinit var email: TextInputLayout
+    private lateinit var birthDate: TextInputLayout
+    private lateinit var password: TextInputLayout
+    private lateinit var passwordConfirm: TextInputLayout
+    private lateinit var btnRegister: Button
+    private lateinit var btnClear: Button
+    private lateinit var signUpLayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        setTitle("Login")
+        setupHyperlink()
+
+        name = findViewById(R.id.ilName)
+        nik = findViewById(R.id.ilNIK)
+        email = findViewById(R.id.ilEmail)
+        birthDate = findViewById(R.id.ilBirthDate)
+        password = findViewById(R.id.ilPassword)
+        passwordConfirm = findViewById(R.id.ilPasswordConfirm)
+        btnRegister = findViewById(R.id.btnSignUp)
+        btnClear = findViewById(R.id.btnClear)
+        signUpLayout = findViewById(R.id.signUpLayout)
+
+        btnClear.setOnClickListener {
+            name.editText?.setText("")
+            nik.editText?.setText("")
+            email.editText?.setText("")
+            birthDate.editText?.setText("")
+            password.editText?.setText("")
+            passwordConfirm.editText?.setText("")
+
+            Snackbar.make(signUpLayout, "Text Cleared Success", Snackbar.LENGTH_LONG).show()
+        }
+
+        btnRegister.setOnClickListener(View.OnClickListener {
+            val Name: String = name.editText?.getText().toString()
+            val Nik: String = nik.editText?.getText().toString()
+            val Email: String = email.editText?.getText().toString()
+            val BirthDate: String = birthDate.editText?.getText().toString()
+            val Password: String = password.editText?.getText().toString()
+            val PasswordConfirm: String = passwordConfirm.editText?.getText().toString()
+
+            var checkSignUp = true
+
+            if(Name.isEmpty()){
+                name.setError("Name must be filled with text")
+                checkSignUp = false
+            }
+
+            if(Nik.isEmpty()){
+                nik.setError("NIK must be filled with text")
+                checkSignUp = false
+            }
+
+            if(Email.isEmpty()){
+                email.setError("E-mail must be filled with text")
+                checkSignUp = false
+            }
+
+            if(BirthDate.isEmpty()){
+                birthDate.setError("Birth Date must be filled with text")
+                checkSignUp = false
+            }
+
+            if(Password.isEmpty()){
+                password.setError("Password must be filled with text")
+                checkSignUp = false
+            }
+
+            if(PasswordConfirm.isEmpty()){
+                passwordConfirm.setError("Password Confirmation must be filled with text")
+                checkSignUp = false
+            }
+
+            if(Password != PasswordConfirm){
+                passwordConfirm.setError("Password Confirmation doesn't match with password")
+                checkSignUp = false
+            }
+            if(Nik.length != 16) {
+                nik.setError("NIK length must be 16 digit")
+            }
+
+            if(checkSignUp == true){
+                val movetoLogin = Intent(this, MainActivity::class.java)
+                startActivity(movetoLogin)
+            }else{
+                return@OnClickListener
+            }
+
+
+        })
+
+    }
+
+    fun setupHyperlink(){
+        val linkTextView = findViewById<TextView>(R.id.btnLogin)
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance())
+
+        linkTextView.setOnClickListener(View.OnClickListener {
+            val movetoLogin = Intent(this, MainActivity::class.java)
+            startActivity(movetoLogin)
+        })
     }
 }
