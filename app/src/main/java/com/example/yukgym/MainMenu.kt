6620faf1682484e394c6a9c3f1ce74b4acd4bb8f
Joshua <yosua2468@gmail.com>
2022-11-03 17:59:13 +0700

    MAPS

--- app/src/main/java/com/example/yukgym/MainMenu.kt
+++ app/src/main/java/com/example/yukgym/MainMenu.kt
@@ -3,16 +3,10 @@ package com.example.yukgym
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//
-
-//
-
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
     }
-
-
 
 }
