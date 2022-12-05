9e60872a6e01b9817c40686f38fc6a15f2c382eb
Joshua <yosua2468@gmail.com>
2022-12-05 11:48:20 +0700

    Unit_testing

---
+++ app/src/main/java/com/example/yukgym/api/HistoryApi.kt
@@ -0,0 +1,15 @@
+package com.example.yukgym
+
+class HistoryApi {
+
+    companion object {
+        val BASE_URL = "https://6368bba315219b84960558d7.mockapi.io/api/"
+
+        val GET_ALL_URL = BASE_URL + "history/"
+        val GET_BY_ID_URL = BASE_URL + "history/"
+        val ADD_URL = BASE_URL + "history"
+        val UPDATE_URL = BASE_URL + "history/"
+        val DELETE_URL = BASE_URL + "history/"
+    }
+
+}
